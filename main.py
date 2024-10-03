0212388e636e1d727d1c3d6ca38742ccd28639fa
Fossai <fossai@gmail.com>
2024-10-03 15:19:34 +0200

    Added the player character and movement

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+#    print(f"Screen width: {SCREEN_WIDTH}")
+#    print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
         
+        player1.update(dt)
+        screen.fill((0,0,0))
+        player1.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        #dt = clock.tick() / 1000
+
 
 
 if __name__ == "__main__":
